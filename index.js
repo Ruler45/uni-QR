ff4b46c9315801e71b532cdd9aaad47f319fa874
ruler45 <sahin0945@gmail.com>
2023-12-22 09:25:58 +0530

    port issue fixed

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const port=process.env.PORT || 4000;
 const app = new express();
 app.use(express.json());
 app.use("/api/url", urlRoute);
 
 app.listen(port, () => {
-    console.log("Server is running on port 4000");
+    console.log("Server is running on port", port);
 });
 
+app.get("/", (req, res) => {
+    res.send("Your API is working perfectly!");
+});
 
 
 connectDB();
